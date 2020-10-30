ff2ea503e43165f0caea3306e96e5fca5b6398da
chaitanya <chaitanyak939@gmail.com>
2020-10-30 11:46:13 +0530

    tasks saved in database and checkbox is added to tasks

--- node.js
+++ node.js
@@ -3,22 +3,56 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var fs = require("fs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
- app.post('/contact', function(req, res){
-    console.log(`data: ${req.body.body.work}`);
-   const existing = fs.readFileSync('./tasks.json',"utf-8");
-    const obj = JSON.parse(existing);
-    obj.push(req.body.body);
-    fs.writeFileSync('./tasks.json',JSON.stringify(obj));
-    res.write(JSON.stringify(req.body));
-    res.end();
-  
- });
-const PORT = process.env.PORT || 3000;
- app.listen(PORT,()=>{
-    console.log(`server started port on ${PORT}`);
-
+app.post("/Addtask", function (req, res) {
+  //console.log( req.body.body);
+  const existing = fs.readFileSync("./tasks.json", "utf-8");
+  const obj = JSON.parse(existing);
+  //console.log(obj)
+  obj.push(req.body.body);
+  fs.writeFileSync("./tasks.json", JSON.stringify(obj, null, 2));
+  res.write(JSON.stringify(req.body));
+  res.end();
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server started port on ${PORT}`);
+});
+
+function readfiledata() {
+  let rawdata = fs.readFileSync("tasks.json");
+  let previous_things = JSON.parse(rawdata);
+  var data_to_render = [];
+  for (let i = 0; i < previous_things.length; i++) {
+    data_to_render.push(Object.values(previous_things[i]));
+  }
+  return data_to_render;
+}
+
+app.get("/retrive_data", (req, res) => {
+  res.write(JSON.stringify(readfiledata()));
+  res.end();
+});
+app.post("/removetask", function (req, res) {
+  const data = req.body.body;
+  const existing = fs.readFileSync("./tasks.json", "utf-8");
+  const dataArray = JSON.parse(existing);
+  dataArray1 = [];
+  for (let i = 0; i < dataArray.length; i++){
+    if(dataArray[i].work != data.work){
+       dataArray1.push(dataArray[i]);
+    }
+  }
+  fs.writeFileSync("./tasks.json", JSON.stringify(dataArray1, null, 2));
+  res.end();
+});
+
+app.post('/task-status',(req,res) =>{
+  console.log(req.body.statusbody);
+  console.log(req.body.taskbody)
+ 
+})
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,114 @@
-function newelement(){
-    var li = document.createElement("li");
-    var inputvalue = document.getElementById("myinput").value;
-    var textnode  = document.createTextNode(inputvalue);
-    li.appendChild(textnode);
-   
-    if(inputvalue===""){
-        alert("write something")
-    }
-    else{
-        document.getElementById("list_of_things_to_do").appendChild(li);   
-    }
-    document.getElementById("myinput").value = "";
-    var span = document.createElement("span")
-    var close = document.createTextNode("\u2715");
-    span.className = "close";
-    span.appendChild(close);
-    span.setAttribute('onClick','alert("closing...")');
-    li.appendChild(span);
-    const Http = new XMLHttpRequest();
-    const url='/contact';
-    Http.open("POST", url);
-    Http.setRequestHeader('Content-Type','application/json');
-    Http.send(JSON.stringify({body:{work:inputvalue}}));
+function newelement() {
+  var li = document.createElement("li");
+  var label = document.createElement("label");
+  var checkbox = document.createElement("input");
 
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.onclick = printStatus;
+
+  var inputvalue = document.getElementById("myinput").value;
+  var textnode = document.createTextNode(inputvalue);
+
+  label.className = "tasks";
+  label.appendChild(checkbox);
+  label.appendChild(textnode);
+  li.appendChild(label);
+
+  if (inputvalue === "") {
+    alert("write something");
+  } else {
+    document.getElementById("list_of_things_to_do").appendChild(li);
+  }
+  document.getElementById("myinput").value = "";
+
+  var span = document.createElement("span");
+  var close = document.createTextNode("\u2715");
+  span.className = "close";
+  span.appendChild(close);
+  span.onclick = remove;
+  li.appendChild(span);
+
+  const Http = new XMLHttpRequest();
+  const url = "/Addtask";
+  Http.open("POST", url);
+  Http.setRequestHeader("Content-Type", "application/json");
+  Http.send(JSON.stringify({ body: { work: inputvalue } }));
+}
+
+const Http1 = new XMLHttpRequest();
+const url = "/retrive_data";
+Http1.open("GET", url);
+Http1.send();
+Http1.onreadystatechange = (event) => {
+  if (Http1.readyState === XMLHttpRequest.DONE) {
+    const retrived_tasks = Http1.responseText;
+    const arr = JSON.parse(retrived_tasks);
+    for (var i = 0; i < arr.length; i++) {
+      renderTask(arr[i]);
+    }
+  }
+};
+
+function renderTask(task) {
+  var li = document.createElement("li");
+  var label = document.createElement("label");
+  var checkbox = document.createElement("input");
+  // var inputvalue = task;
+  var textnode = document.createTextNode(task);
+  var span = document.createElement("span");
+  var close = document.createTextNode("\u2715");
+
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.onclick = printStatus;
+
+  label.className = "tasks";
+  label.appendChild(checkbox);
+  label.appendChild(textnode);
+
+  span.className = "close";
+  span.appendChild(close);
+  span.onclick = remove;
+
+  li.appendChild(label);
+  li.appendChild(span);
+  document.getElementById("list_of_things_to_do").appendChild(li);
+}
+
+function remove(event) {
+  const ul = event.currentTarget.parentElement.parentElement;
+  const li = event.currentTarget.parentElement;
+  const label = li.getElementsByTagName("label");
+  const Http2 = new XMLHttpRequest();
+  const url = "/removetask";
+  Http2.open("POST", url);
+  Http2.setRequestHeader("Content-Type", "application/json");
+  Http2.send(
+    JSON.stringify({
+      body: {
+        work: label[0].innerText,
+      },
+    })
+  );
+  ul.removeChild(li);
+}
+
+function printStatus(event) {
+  const li = event.currentTarget.parentElement;
+  const checked_value = event.currentTarget;
+  const state = checked_value.checked;
+  console.log(JSON.stringify({ body: { work: li.innerText } }));
+  const Http3 = new XMLHttpRequest();
+  const url = "/task-status";
+  Http3.open("POST", url);
+  Http3.setRequestHeader("Content-Type", "application/json");
+  Http3.send(
+    JSON.stringify({
+      statusbody: {
+        status: state,
+      },
+      taskbody: {
+        work: li.innerText,
+      },
+    })
+  );
 }
